c1f1a3d35a8a82819555175384f5ac42870072c1
SiShanZI <SueLitera@Outlook.com>
2017-08-08 15:59:07 +0800

    Pick up C

--- Sue_Arithmetic_EC.c
+++ Sue_Arithmetic_EC.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h> /*gcc -lm*/
+#include <ctype.h> /*isdigit, isspace*/
 
 #define TAG '\n'
 #define MAX 250
@@ -9,8 +12,8 @@ int main(void)
   char tmp, msg[MAX];
   for(i = 0; i < MAX; i++){msg[i] = '\0';}
 
-  for(i = 0; (tmp = getchar()) != TAG && i < MAX; i++){msg[i] = tmp;}
-  printf("%s\n", msg);
+  for(i = 0; i < MAX && (tmp = getchar()) != TAG; i++){msg[i] = tmp;}
+  printf("%s\n%lu\n%f\n", msg, strlen(msg), sin((double)strlen(msg)));
 
   return 0;
 }
